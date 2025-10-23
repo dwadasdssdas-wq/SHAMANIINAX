d83da5b9fe27bfa1aa5538cf0c3dd94695f5a9ca
dwadasdssdas-wq <dwadasdssdas@gmail.com>
2025-10-23 17:23:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
 const SHAMAN_AI_CONFIG = {
-    HUGGING_FACE_TOKEN: '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',
+    HUGGING_FACE_TOKEN: window.SHAMAN_AI_ENV?.HUGGING_FACE_TOKEN || '',
     MODELS: {
         CHAT: 'microsoft/DialoGPT-medium',
         MATH: 'google/flan-t5-base',
-        GENERAL: 'microsoft/DialoGPT-small'
+        GENERAL: 'microsoft/DialoGPT-medium'
     },
     API_URLS: {
         HUGGING_FACE: 'https://api-inference.huggingface.co/models/'
     }
 };
-
-
-
-
